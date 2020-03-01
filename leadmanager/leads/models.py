6b28148d60d1a7d490043296178b7499da4e8aa2
Hao Hao <hhao1@ualberta.ca>
2020-03-01 15:44:45 -0800

    first commit

---
+++ leadmanager/leads/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Lead(models.Model):
+    Country_Name=models.CharField(max_length=100)
+    Vaccine=models.CharField(max_length=100,blank=True)
+    #email=models.EmailField(max_length=100,unique=True)
+    message=models.CharField(max_length=500,blank=True)#optional
+
+# Create your models here.
