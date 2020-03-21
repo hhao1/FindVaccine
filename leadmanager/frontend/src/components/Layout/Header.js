7997b1074ec661bdefa44fd8400ddda4e550ef5d
Hao Hao <hhao1@ualberta.ca>
2020-03-21 12:43:04 -0700

    fixed core problem

--- leadmanager/frontend/src/components/Layout/Header.js
+++ leadmanager/frontend/src/components/Layout/Header.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import {
   Navbar,
   NavbarBrand,
@@ -13,17 +13,16 @@ import {
   DropdownItem,
   Button,
   NavbarText
-} from 'reactstrap'
-import { withRouter } from 'react-router-dom'
+} from "reactstrap";
+import { withRouter } from "react-router-dom";
 
-import { getLeads } from "../../actions/leads"
+import { getLeads } from "../../actions/leads";
 
 export class Header extends Component {
+  constructor(props) {
+    super(props);
 
-  constructor(props){
-    super(props)
-
-    this.handleDropdownItemClick = this.handleDropdownItemClick.bind(this)
+    this.handleDropdownItemClick = this.handleDropdownItemClick.bind(this);
   }
 
   static propTypes = {
@@ -31,48 +30,64 @@ export class Header extends Component {
   };
 
   componentDidMount() {
-      this.props.getLeads()
+    this.props.getLeads();
   }
 
   // shoud update Redux store field
   handleDropdownItemClick(e) {
-    console.log(e.target.textContent)
-
+    console.log(e.target.textContent);
   }
 
   render() {
-
     // const onInfoPage this.props.curDestination !== "" ? true : false
 
-    const {
-      leads
-    } = this.props
+    const { leads } = this.props;
 
-    const onInfoPage = true
+    const onInfoPage = true;
 
     return (
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">Vaccine Finder</NavbarBrand>
         <Nav>
-          <NavItem><NavLink href="/">Home</NavLink></NavItem>
-          <NavItem><NavLink href="/info">About us</NavLink></NavItem>
-          <NavItem><NavLink href="/">Contact Us</NavLink></NavItem>
+          <NavItem>
+            <NavLink href="/">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/">About us</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/">Contact Us</NavLink>
+          </NavItem>
 
-          {onInfoPage && 
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
+          {onInfoPage && (
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
                 Change Destination
-            </DropdownToggle>
-            <DropdownMenu right>
-
-              <input type="text" name="zipcode" placeholder="Enter zipcode"></input>
-              <DropdownItem divider />
-              {leads.map( lead => <DropdownItem key={lead.id} onClick={this.handleDropdownItemClick}>{lead.Country_Name}</DropdownItem>)}
-            </DropdownMenu>
-          </UncontrolledDropdown>}
+              </DropdownToggle>
+              <DropdownMenu right>
+                <input
+                  type="text"
+                  name="zipcode"
+                  placeholder="Enter zipcode"
+                ></input>
+                <DropdownItem divider />
+                {leads.map(lead => (
+                  <DropdownItem
+                    key={lead.id}
+                    onClick={this.handleDropdownItemClick}
+                  >
+                    {lead.Country_Name}
+                  </DropdownItem>
+                ))}
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          )}
         </Nav>
 
-        <img className="float-right" src="https://www.countryflags.io/be/shiny/32.png"></img>
+        <img
+          className="float-right"
+          src="https://www.countryflags.io/be/shiny/32.png"
+        ></img>
       </Navbar>
     );
   }
@@ -82,4 +97,4 @@ const mapStateToProps = state => ({
   leads: state.leadReducer.leads
 });
 
-export default connect(mapStateToProps, {getLeads})(Header);
+export default connect(mapStateToProps, { getLeads })(Header);
--- scrapy_database/scrapy_database/spiders/country.py
+++ scrapy_database/scrapy_database/spiders/country.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import urllib
 
 
 class CountrySpider(scrapy.Spider):
     name = 'country'
     allowed_domains = ['https://travel.gc.ca/']
     start_urls = [
-        'https://travel.gc.ca/destinations/afghanistan#health']
+        'https://travel.gc.ca/travelling/health-safety/vaccines']
 
     def parse(self, response):
-        selectors = response.xpath('//details/details/summary')
-        print(selectors)
+        selectors = response.xpath('//details/details/summary').extract()
+        # print(selectors)
+        namelist = response.xpath('//details/details/summary').extract()
+        detaillist = response.xpath("//details/details/").extract()
+        listlength = len(namelist)
+        item = {}
+        for i in range(0, listlength):
+            item['Name'] = namelist[i]
+            item['Vaccine'] = htmllist[i]
+
+            urllib.request.urlretrieve(
+                "https://travel.gc.ca/destinations/"+str(list[i]))
+            yield item
